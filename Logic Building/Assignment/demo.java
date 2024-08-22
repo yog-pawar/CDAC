5dea57893d855468b2ace4d38e98b29d40291027
yog-pawar <yog.pawar16595@gmail.com>
2024-08-22 16:25:04 +0530

    Add files via upload

---
+++ Logic Building/Assignment/demo.java
@@ -0,0 +1,66 @@
+class posneg{
+
+
+public static void main (String args[]){
+
+int a=-2;
+
+if(a>0)
+ 
+System.out.println("The number is positive");
+
+else
+
+System.out.println("The number is negative");
+
+}
+
+}
+
+
+
+class largenumber{
+
+
+public static void main (String args[]){
+
+int a=5;
+int b=10;
+int c=15;
+
+if(a>b && b>c)
+ 
+System.out.println("The largest number is a"+a);
+
+else if(b>a && a>c)
+
+System.out.println("The largest number is b"+b);
+
+else
+System.out.println("The largest number is c"+c);
+
+}
+
+}
+
+
+
+class time{
+
+
+public static void main (String args[]){
+
+int a=8;
+
+if(5<8 && 8<12)
+ 
+System.out.println("good morning");
+
+else
+
+System.out.println("have a good day");
+
+}
+
+}
+
---
+++ Logic Building/Assignment/oddeven.java
@@ -0,0 +1,50 @@
+class oddeven{
+
+
+public static void main (String args[]){
+
+int a=25;
+
+if(a%2==0)
+ 
+System.out.println("The number is positive");
+
+else
+
+System.out.println("The number is negative");
+
+}
+
+}
+
+
+class square{
+
+public static void main (String args[]){
+
+int side= 10;
+
+int area= side*side;
+ 
+System.out.println("The area of square is area" +area);
+
+}
+
+}
+
+
+class rectangle{
+
+public static void main (String args[]){
+
+int length= 10;
+
+int width=20;
+
+int area=length*width;
+ 
+System.out.println("The area of rectangle is area" +area);
+
+}
+
+}
