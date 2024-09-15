f162fc5b5f324b2cec23b2fa98c49b0cb478f393
yog-pawar <yog.pawar16595@gmail.com>
2024-09-15 16:00:50 +0530

    Add files via upload

---
+++ Module OOPJ/MaamAssignments/Assignment5/Question4/Main.java
@@ -0,0 +1,21 @@
+package assign5_4;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Student s = new Student();
+		System.out.println("name is "+s.name);
+		System.out.println("roll number is "+Student.rollno);
+		System.out.println("Subject is "+Student.sub);
+		System.out.println("percent is "+s.percent);
+	
+		
+		
+	    
+	    
+		
+		
+		
+	}
+
+}
---
+++ Module OOPJ/MaamAssignments/Assignment5/Question4/Student.java
@@ -0,0 +1,10 @@
+package assign5_4;
+
+class Student {
+		public String name = "Yogesh";
+		public static int rollno =10;
+		public static String sub= "java";
+		public double percent = 80.80;
+
+		
+}
